bdd8c1fd25438d9d7fb9408c381f185b3f6113e7
Shadow™ <30982214+ShadowKA@users.noreply.github.com>
2017-09-15 19:39:06 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,8 @@
 
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const r = require('rethinkdb');
+const Dash = require('rethinkdbdash')
+const r = new Dash()
 const prefix = "."
 
 client.on('ready', () => {
