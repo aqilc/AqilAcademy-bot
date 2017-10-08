fda97d1c6ca9aa559f59ad3bed921d6f3de2f960
Shadow™ <30982214+ShadowKA@users.noreply.github.com>
2017-10-08 18:47:42 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js");
-const client = new Discord.Client();
+const client = new Discord.Client({autoReconnect:true});
 const ms = require("ms")
 const prefix = "."
 var electionOn;
