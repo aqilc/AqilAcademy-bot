a04a3e59762fc76e647c1db68699077a4d1178a4
Shadow™ <30982214+ShadowKA@users.noreply.github.com>
2017-10-11 18:05:50 -0400

    Modify Hello Command

--- bot.js
+++ bot.js
@@ -908,7 +908,6 @@ if(said.startsWith(prefix + "ban")) {
     }
 }
   if(said.startsWith(prefix + "hello")) {
-    msg.delete(3000);
     msg.reply("Hey!");
   }
 if(said.startsWith(prefix + "warn")) {
