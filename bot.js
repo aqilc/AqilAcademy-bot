9985cc2f5e37e456c2bb7c0307e035b9e2dee6da
AqilCont <32044067+AqilCont@users.noreply.github.com>
2017-11-26 14:10:50 -0500

    Stuff
    I unblocked `.cookpie`

--- bot.js
+++ bot.js
@@ -57,7 +57,7 @@ client.on('message', msg => {
     
 var said = msg.content.toLowerCase(); //declare said
     if (msg.content.startsWith(prefix + "cookpie")) {
-          /*const responses = ["burnt", "perfect", "undercooked"]
+          const responses = ["burnt", "perfect", "undercooked"]
                   let toSay = Math.floor(Math.random() * responses.length)
                   console.log(toSay)
                   if(toSay === 0) {
@@ -109,8 +109,7 @@ var said = msg.content.toLowerCase(); //declare said
                       setTimeout(function() {
                         msg.reply("Oh no! Your pie was undercooked!")
                       }, 3000)
-                  }*/
-        msg.reply("Something went wrong. Please try again.")
+                  }
       }
     if(msg.content.startsWith(prefix + "tag")) {
         let saidTag = said.split(" ")
